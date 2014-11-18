1d9cef2b3677f893f8e252d2b417bf7b71e9f3ba
luisgiltajamar <luis.gil@tajamar365.com>
2014-11-18 12:42:03 +0100

    a

---
+++ CursosMVC/Controllers/ProfesorController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CursosMVC.Models;
+
+namespace CursosMVC.Controllers
+{
+    public class ProfesorController : Controller
+    {
+        private DatosEntities db = new DatosEntities();
+       
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.ActionDescriptor.ActionName == "Index")
+            {
+                ViewBag.Eso = "Index";
+            }
+
+            var url = Server.MapPath("~/logs");
+
+            if (!System.IO.Directory.Exists(url))
+                System.IO.Directory.CreateDirectory(url);
+            
+            System.IO.File.AppendAllText(url+"/log.txt",String.Format(
+                "En el controlador {0} y la accion {1}",
+                filterContext.Controller.ToString(),
+                filterContext.ActionDescriptor.ActionName));
+
+        }
+
+
+        // GET: Profesor
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Listado de profesores";
+            ViewBag.Fecha = DateTime.Now;
+
+            return View(db.Profesor.ToList());
+        }
+
+        // GET: Profesor/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = db.Profesor.Find(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // GET: Profesor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profesor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idProfesor,nombre,experiencia")] Profesor profesor)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                db.Profesor.Add(profesor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(profesor);
+        }
+
+        // GET: Profesor/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = db.Profesor.Find(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // POST: Profesor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idProfesor,nombre,experiencia")] Profesor profesor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(profesor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(profesor);
+        }
+
+        // GET: Profesor/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = db.Profesor.Find(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // POST: Profesor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Profesor profesor = db.Profesor.Find(id);
+            db.Profesor.Remove(profesor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CursosMVC/Validadores/ValidadorExperiencia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CursosMVC.Validadores
+{
+    public class ValidadorExperiencia:ValidationAttribute
+    {
+        public ValidadorExperiencia()
+        {
+            ErrorMessage = "La experiencia introducida no es válida";
+        }
+
+        public override bool IsValid(object value)
+        {
+            return Convert.ToInt32(value)%3 == 0;
+
+        }
+    }
+    public class ValidadorExperienciaDos : ValidationAttribute
+    {
+        public ValidadorExperienciaDos()
+        {
+            ErrorMessage = "La experiencia introducida no es válida";
+        }
+
+        public override bool IsValid(object value)
+        {
+            return Convert.ToInt32(value) % 2 == 0;
+
+        }
+    }
+}
